3bab27210ac8b4f946ff4a3f43454126226cce69
Hyukjin Kwon <hjnim0909@gmail.com>
2021-12-23 18:23:04 +0900

    JS: Navbar clicking menus
    - Handling scrolling when tapping the menus
    
    - Fixed CSS
    : assigned z-index for #navbar to be on top as always (not to be diturbed by the project imgs)

--- main.js
+++ main.js
@@ -10,3 +10,17 @@ document.addEventListener("scroll", () => {
     navbar.classList.remove("navbar--dark");
   }
 });
+
+// Handling scrolling when tapping the menus
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  } else {
+    console.log(event.target.dataset.link);
+  }
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+});
