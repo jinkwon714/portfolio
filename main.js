39f8648af09a4279091b95c70d620bae6cbfdfae
Hyukjin Kwon <hjnim0909@gmail.com>
2021-12-23 17:46:41 +0900

    JS: Navbar
    Making navbar transparent for fixing on the top

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
-.navbar__logo__image{
-  width:10px;
-}
+"use strict";
+
+// Making navbar transparent for fixing on the top
+const navbar = document.querySelector("#navbar");
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add("navbar--dark");
+  } else {
+    navbar.classList.remove("navbar--dark");
+  }
+});
